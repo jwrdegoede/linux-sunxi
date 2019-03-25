c2e83351b8bc11cc82a7b9305b2e607615ddd55b
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2019-03-25 13:32:46 +0300

    drm/i915: Associate ACPI connector nodes with connector entries
    On Intel platforms we know that the ACPI connector device
    node order will follow the order the driver (i915) decides.
    The decision is made using the custom Intel ACPI OpRegion
    (intel_opregion.c), though the driver does not actually know
    that the values it sends to ACPI there are used for
    associating a device node for the connectors, and assigning
    address for them.
    
    In reality that custom Intel ACPI OpRegion actually violates
    ACPI specification (we supply dynamic information to objects
    that are defined static, for example _ADR), however, it
    makes assigning correct connector node for a connector entry
    straightforward (it's one-on-one mapping).
    
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    [hdegoede@redhat.com: Move intel_acpi_assign_connector_fwnodes() to
     intel_acpi.c]
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/gpu/drm/i915/display/intel_acpi.c
+++ drivers/gpu/drm/i915/display/intel_acpi.c
@@ -263,3 +263,43 @@ void intel_acpi_device_id_update(struct drm_i915_private *dev_priv)
 	}
 	drm_connector_list_iter_end(&conn_iter);
 }
+
+/* NOTE: The connector order must be final before this is called. */
+void intel_acpi_assign_connector_fwnodes(struct drm_i915_private *i915)
+{
+	struct drm_connector_list_iter conn_iter;
+	struct drm_device *drm_dev = &i915->drm;
+	struct device *kdev = &drm_dev->pdev->dev;
+	struct fwnode_handle *fwnode = NULL;
+	struct drm_connector *connector;
+	struct acpi_device *adev;
+
+	drm_connector_list_iter_begin(drm_dev, &conn_iter);
+	drm_for_each_connector_iter(connector, &conn_iter) {
+		/* Always getting the next, even when the last was not used. */
+		fwnode = device_get_next_child_node(kdev, fwnode);
+		if (!fwnode)
+			break;
+
+		switch (connector->connector_type) {
+		case DRM_MODE_CONNECTOR_LVDS:
+		case DRM_MODE_CONNECTOR_eDP:
+		case DRM_MODE_CONNECTOR_DSI:
+			/*
+			 * Integrated displays have a specific address 0x1f on
+			 * most Intel platforms, but not on all of them.
+			 */
+			adev = acpi_find_child_device(ACPI_COMPANION(kdev),
+						      0x1f, 0);
+			if (adev) {
+				connector->fwnode = acpi_fwnode_handle(adev);
+				break;
+			}
+			fallthrough;
+		default:
+			connector->fwnode = fwnode;
+			break;
+		}
+	}
+	drm_connector_list_iter_end(&conn_iter);
+}
