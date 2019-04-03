7c593b1797fa4283562571bb298cd8e4ee6ff21a
sfumato77 <sfumato1977@gmail.com>
2019-04-03 19:40:34 +0200

    FIX: Warning message
    Kernel 5.1.0-rc3+  32bit  intel Bay Trail
    
    [    0.390281] gpio gpiochip1: (INT33FC:01): detected irqchip that is shared with multiple gpiochips: please fix the driver.
    [    0.390738] gpio gpiochip2: (INT33FC:02): detected irqchip that is shared with multiple gpiochips: please fix the driver.

--- drivers/pinctrl/intel/pinctrl-baytrail.c
+++ drivers/pinctrl/intel/pinctrl-baytrail.c
@@ -194,6 +194,7 @@ struct byt_pinctrl_soc_data {
 
 struct byt_gpio {
 	struct gpio_chip chip;
+	struct irq_chip irq_chip;
 	struct platform_device *pdev;
 	struct pinctrl_dev *pctl_dev;
 	struct pinctrl_desc pctl_desc;
@@ -1593,15 +1594,6 @@ static int byt_irq_type(struct irq_data *d, unsigned int type)
 	return 0;
 }
 
-static struct irq_chip byt_irqchip = {
-	.name		= "BYT-GPIO",
-	.irq_ack	= byt_irq_ack,
-	.irq_mask	= byt_irq_mask,
-	.irq_unmask	= byt_irq_unmask,
-	.irq_set_type	= byt_irq_type,
-	.flags		= IRQCHIP_SKIP_SET_WAKE,
-};
-
 static void byt_gpio_irq_handler(struct irq_desc *desc)
 {
 	struct irq_data *data = irq_desc_get_irq_data(desc);
@@ -1727,15 +1719,23 @@ static int byt_gpio_probe(struct byt_gpio *vg)
 	/* set up interrupts  */
 	irq_rc = platform_get_resource(vg->pdev, IORESOURCE_IRQ, 0);
 	if (irq_rc && irq_rc->start) {
+		
+		vg->irq_chip.name       = "BYT-GPIO";
+		vg->irq_chip.irq_ack    = byt_irq_ack,
+		vg->irq_chip.irq_mask   = byt_irq_mask,
+		vg->irq_chip.irq_unmask = byt_irq_unmask,
+		vg->irq_chip.irq_set_type       = byt_irq_type,
+		vg->irq_chip.flags      = IRQCHIP_SKIP_SET_WAKE,
+		
 		byt_gpio_irq_init_hw(vg);
-		ret = gpiochip_irqchip_add(gc, &byt_irqchip, 0,
+		ret = gpiochip_irqchip_add(gc, &vg->irq_chip, 0,
 					   handle_bad_irq, IRQ_TYPE_NONE);
 		if (ret) {
 			dev_err(&vg->pdev->dev, "failed to add irqchip\n");
 			return ret;
 		}
 
-		gpiochip_set_chained_irqchip(gc, &byt_irqchip,
+		gpiochip_set_chained_irqchip(gc, &vg->irq_chip,
 					     (unsigned)irq_rc->start,
 					     byt_gpio_irq_handler);
 	}
