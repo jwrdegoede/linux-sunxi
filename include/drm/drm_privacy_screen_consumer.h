fbbd300611f6555ad994d8e71bafc863effd8f56
Hans de Goede <hdegoede@redhat.com>
2020-05-26 22:55:39 +0200

    drm: Add privacy-screen class
    On some new laptops the LCD panel has a builtin electronic privacy-screen.
    We want to export this functionality as a property on the drm connector
    object. But often this functionality is not exposed on the GPU but on some
    other (ACPI) device.
    
    This commit adds a privacy-screen class allowing the driver for these
    other devices to register themselves as a privacy-screen provider; and
    allowing the drm/kms code to get a privacy-screen provider associated
    with a specific GPU/connector combo.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

---
+++ include/drm/drm_privacy_screen_consumer.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright (C) 2020 Red Hat, Inc.
+ *
+ * Authors:
+ * Hans de Goede <hdegoede@redhat.com>
+ */
+
+#ifndef __DRM_PRIVACY_SCREEN_CONSUMER_H__
+#define __DRM_PRIVACY_SCREEN_CONSUMER_H__
+
+#include <linux/device.h>
+#include <drm/drm_connector.h>
+
+struct drm_privacy_screen;
+
+struct drm_privacy_screen *drm_privacy_screen_get(struct device *dev,
+						  const char *con_id);
+void drm_privacy_screen_put(struct drm_privacy_screen *priv);
+
+int drm_privacy_screen_set_sw_state(struct drm_privacy_screen *priv,
+				    enum drm_privacy_screen_status sw_state);
+void drm_privacy_screen_get_state(struct drm_privacy_screen *priv,
+				  enum drm_privacy_screen_status *sw_state_ret,
+				  enum drm_privacy_screen_status *hw_state_ret);
+
+#endif
