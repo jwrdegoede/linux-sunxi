6e2e91a93cfe82964a164e2862e19a77e49788cd
Yauhen Kharuzhy <jekhor@gmail.com>
2020-12-17 02:17:49 +0300

    Revert "ACPI / x86: Make PWM2 device always present at Lenovo Yoga Book"
    Keyboard backlight is handled by lenovo-yogabook-wmi driver now.
    
    This reverts commit ff6cdfd71495e2b7995b6ec95480d15f89e3beef.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/acpi/x86/utils.c
+++ drivers/acpi/x86/utils.c
@@ -54,10 +54,6 @@ static const struct always_present_id always_present_ids[] = {
 	ENTRY("80860F09", "1", X86_MATCH(ATOM_SILVERMONT), {}),
 	ENTRY("80862288", "1", X86_MATCH(ATOM_AIRMONT), {}),
 
-	/* Lenovo Yoga Book uses PWM2 for keyboard backlight control */
-	ENTRY("80862289", "2", X86_MATCH(ATOM_AIRMONT), {
-			DMI_MATCH(DMI_PRODUCT_NAME, "Lenovo YB1-X9"),
-		}),
 	/* The Xiaomi Mi Pad 2 uses PWM2 for touchkeys backlight control */
 	ENTRY("80862289", "2", X86_MATCH(ATOM_AIRMONT), {
 		DMI_MATCH(DMI_SYS_VENDOR, "Xiaomi Inc"),
