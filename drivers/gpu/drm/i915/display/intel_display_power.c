594e40daf3a63c33c57d779e5d25d50ba4324859
Hans de Goede <hdegoede@redhat.com>
2021-10-24 14:27:38 +0200

    drm/i915: Add NO_VLV_DISP_PW_DPIO_CMN_BC_INIT quirk
    Add a NO_VLV_DISP_PW_DPIO_CMN_BC_INIT quirk to fix i915 not working on
    the Xiaomi Mi Pad 2 (with CHT x5-Z8500 SoC).
    
    The Xiaomi Mi Pad 2 uses quite an unusual hardware-design for a Cherry
    Trail tablet. It deviates from the typical reference design based tablets
    in many ways.
    
    The Mi Pad 2 does not have any DisplayPort or HDMI outouts. I suspect that
    as part of its unusual design it also has some supply rail which is only
    used for DisplayPort or HDMI not connected.
    
    Force-enabling the dpio-common-bc powerwell as the i915 normal does at boot
    appears to cause the P-Unit to hang. When booting with a serial-usb console
    the following errors are logged before the system freezes:
    
     i915 0000:00:02.0: [drm] *ERROR* timeout setting power well state 00000000 (fffff3ff)
     i915 0000:00:02.0: [drm] *ERROR* Display PHY 0 is not power up
     ------------[ cut here ]------------
     i915 0000:00:02.0: DPIO read pipe A reg 0x8170 == 0xffffffff
     WARNING: CPU: 3 PID: 258 at drivers/gpu/drm/i915/intel_sideband.c:257 vlv_dpio_read+0x95/0xb0 [i915]
     ...
     Call Trace:
      chv_dpio_cmn_power_well_enable+0xab/0x210 [i915]
      __intel_display_power_get_domain.part.0+0xa0/0xc0 [i915]
      intel_power_domains_init_hw+0x26d/0x760 [i915]
      intel_modeset_init_noirq+0x5d/0x270 [i915]
      i915_driver_probe+0x6b6/0xd10 [i915]
      ...
    
    If I disable the WARN about the register being 0xffffffff, so that the
    system can log some more dmesg output over the serial console before
    freezing, the following errors are also logged:
    
     i915 0000:00:02.0: [drm] *ERROR* timeout setting power well state 00000000 (fcfff3ff)
     i915 0000:00:02.0: [drm] *ERROR* Display PHY 1 is not power up
    
    With this patch to disable the force-enabling of the PHY 0 / dpio-common-bc
    powerwell in place, this error for PHY 1 goes away. So it seems that trying
    the force-enabling of the PHY 0 / dpio-common-bc powerwell freezes the
    P-Unit, causing the subsequent enabling of PHY 1 to also fail (and causing
    the entire system to freeze within seconds).
    
    With this patch the PHY 1 error disappears and the entire system works.
    
    Note this change also moves the intel_init_quirks() call a bit up inside
    intel_modeset_init_noirq() this is necessary so that the quirk is set
    before the intel_power_domains_init_hw() call. This is harmless, all that
    intel_init_quirks() does is set some bits in drm_i915_private.quirks and
    make some drm_info() log calls.
    
    Reported-by: Tsuchiya Yuto <kitakar@gmail.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/gpu/drm/i915/display/intel_display_power.c
+++ drivers/gpu/drm/i915/display/intel_display_power.c
@@ -19,6 +19,7 @@
 #include "intel_mchbar_regs.h"
 #include "intel_pch_refclk.h"
 #include "intel_pcode.h"
+#include "intel_quirks.h"
 #include "intel_snps_phy.h"
 #include "skl_watermark.h"
 #include "vlv_sideband.h"
@@ -471,6 +472,12 @@ out_verify:
 	return ret;
 }
 
+static const struct i915_power_well_instance *
+i915_power_well_instance(const struct i915_power_well *power_well)
+{
+	return &power_well->desc->instances->list[power_well->instance_idx];
+}
+
 static void
 __intel_display_power_get_domain(struct drm_i915_private *dev_priv,
 				 enum intel_display_power_domain domain)
@@ -481,8 +488,14 @@ __intel_display_power_get_domain(struct drm_i915_private *dev_priv,
 	if (intel_display_power_grab_async_put_ref(dev_priv, domain))
 		return;
 
-	for_each_power_domain_well(dev_priv, power_well, domain)
+	for_each_power_domain_well(dev_priv, power_well, domain) {
+		if (domain == POWER_DOMAIN_INIT &&
+		    intel_has_quirk(dev_priv, QUIRK_NO_VLV_DISP_PW_DPIO_CMN_BC_INIT) &&
+		    i915_power_well_instance(power_well)->id == VLV_DISP_PW_DPIO_CMN_BC)
+			continue;
+
 		intel_power_well_get(dev_priv, power_well);
+	}
 
 	power_domains->domain_use_count[domain]++;
 }
@@ -577,8 +590,14 @@ __intel_display_power_put_domain(struct drm_i915_private *dev_priv,
 
 	power_domains->domain_use_count[domain]--;
 
-	for_each_power_domain_well_reverse(dev_priv, power_well, domain)
+	for_each_power_domain_well_reverse(dev_priv, power_well, domain) {
+		if (domain == POWER_DOMAIN_INIT &&
+		    intel_has_quirk(dev_priv, QUIRK_NO_VLV_DISP_PW_DPIO_CMN_BC_INIT) &&
+		    i915_power_well_instance(power_well)->id == VLV_DISP_PW_DPIO_CMN_BC)
+			continue;
+
 		intel_power_well_put(dev_priv, power_well);
+	}
 }
 
 static void __intel_display_power_put(struct drm_i915_private *dev_priv,
