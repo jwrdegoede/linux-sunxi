adad0db3cfc40a896a17e877141bac4cf240a428
Hans de Goede <hdegoede@redhat.com>
2022-12-12 22:19:07 +0100

    media: atomisp: Remove custom ATOMISP_IOC_G_SENSOR_MODE_DATA ioctl
    This ioctl returns a number of fixed sensor parameters +
    a number of mode-specific parameters.
    
    With libcamera these fixed parameters are instead stored in a table
    with sensor-name to parameters mappings (camera_sensor_properties.cpp);
    and the variable parameters can be derived from the set fmt.
    
    So this custom ioctl is not necessary; and it currently has no users.
    
    Remove the ioctl and all the sensor drivers xxxx_get_intg_factor()
    helpers which return this info.
    
    This is part of a patch-series which tries to remove atomisp specific /
    custom code from the sensor drivers, with as end goal to make the atomisp
    drivers regular camera sensor drivers.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/staging/media/atomisp/i2c/ov2722.h
+++ drivers/staging/media/atomisp/i2c/ov2722.h
@@ -202,7 +202,6 @@ struct ov2722_device {
 
 	struct camera_sensor_platform_data *platform_data;
 	int power_on;
-	int vt_pix_clk_freq_mhz;
 	int run_mode;
 	u16 pixels_per_line;
 	u16 lines_per_frame;
