004ac8db26249fd8995740a2bdb079e1d75bd71e
Hans de Goede <hdegoede@redhat.com>
2023-02-18 16:02:39 +0100

    media: atomisp: Remove yuvpp_mode
    yuvpp_mode is never set, so it always is false remove it.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/staging/media/atomisp/pci/atomisp_compat_css20.c
+++ drivers/staging/media/atomisp/pci/atomisp_compat_css20.c
@@ -2435,8 +2435,6 @@ static unsigned int atomisp_get_pipe_index(struct atomisp_sub_device *asd,
 
 	switch (source_pad) {
 	case ATOMISP_SUBDEV_PAD_SOURCE_VIDEO:
-		if (asd->yuvpp_mode)
-			return IA_CSS_PIPE_ID_YUVPP;
 		if (asd->copy_mode)
 			return IA_CSS_PIPE_ID_COPY;
 		if (asd->run_mode->val == ATOMISP_RUN_MODE_VIDEO
@@ -2455,8 +2453,6 @@ static unsigned int atomisp_get_pipe_index(struct atomisp_sub_device *asd,
 		}
 		fallthrough;
 	case ATOMISP_SUBDEV_PAD_SOURCE_PREVIEW:
-		if (asd->yuvpp_mode)
-			return IA_CSS_PIPE_ID_YUVPP;
 		if (asd->copy_mode)
 			return IA_CSS_PIPE_ID_COPY;
 		if (asd->run_mode->val == ATOMISP_RUN_MODE_VIDEO)
