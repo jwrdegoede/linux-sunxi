83684b989d37bf119cf5fc74e2ac551e23a9a2ce
Hans de Goede <hdegoede@redhat.com>
2024-09-01 13:18:51 +0200

    media: v4l: Call s_stream() on VCM when it is called on the associated sensor
    Currently VCM drivers power-up the VCM as soon as the VCM's /dev node
    is opened and through the runtime-pm device-link to the sensor this
    also powers up the sensor.
    
    Powering up the VCM and sensor when the /dev node is opened is undesirable,
    without a VCM sensors delay powering up until s_stream(1) is called. This
    allows querying / negotiating capabilities without powering things up.
    
    Sometimes a long running daemon like pipewire may keep the /dev node open
    all the time. The kernel should still be able to powerdown the VCM + sensor
    in this scenario.
    
    VCM drivers should be able to do the same as sensor drivers and only
    power-up the VCM when s_stream(1) is called on the VCM subdev, but this
    requires that s_stream() is actually called on the VCM when the sensor
    starts / stops streaming.
    
    The s_stream() call on sensor subdevs is done by CSI-receiver/ISP drivers.
    To avoid needing to change all these call sites to also call s_stream()
    on the VCM (if there is one) handle the VCM in the v4l2-core similar to how
    the core takes care of turning on/off the privacy LED.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- include/media/v4l2-subdev.h
+++ include/media/v4l2-subdev.h
@@ -1103,6 +1103,7 @@ struct v4l2_subdev {
 	 */
 
 	struct led_classdev *privacy_led;
+	struct v4l2_subdev *vcm;
 
 	/*
 	 * TODO: active_state should most likely be changed from a pointer to an
@@ -1113,6 +1114,7 @@ struct v4l2_subdev {
 	struct v4l2_subdev_state *active_state;
 	u64 enabled_pads;
 	bool s_stream_enabled;
+	bool vcm_enabled;
 };
 
 
