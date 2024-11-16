d42c00fdc564dae67026e8f062e89576e5043bc7
Hans de Goede <hdegoede@redhat.com>
2024-11-16 21:05:43 +0100

    platform/x86: x86-android-tablets: Add support for Acer A1-840 tablet
    Add support for the Acer Iconia One 8 A1-840 (non FHD version) tablet.
    
    This tablet has the usual issues for tablets shipped with Android as
    factory OS. The DSDT is broken in various ways, so i2c_clients for
    various devices as well as the INT3496 platform-device for OTG extcon
    handling need to be instantiated manually by x86-android-tablets.
    
    This table is special in that it is the first time a Bay Trail device
    has been found to use the Dollar Cove TI PMIC and the first time that
    the PMIC's Coulomb Counter is used as fuel-gauge.
    
    So far this PMIC has only been used together with Cherry Trail SoCs
    and always in combination with a separate full-featured fuel-gauge IC.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/platform/x86/x86-android-tablets/acer.c
+++ drivers/platform/x86/x86-android-tablets/acer.c
@@ -15,6 +15,159 @@
 #include "shared-psy-info.h"
 #include "x86-android-tablets.h"
 
+/* Acer Iconia One 8 A1-840 (non FHD version) */
+static const struct property_entry acer_a1_840_4v20_battery_props[] = {
+	PROPERTY_ENTRY_STRING("compatible", "simple-battery"),
+	PROPERTY_ENTRY_STRING("device-chemistry", "lithium-ion-polymer"),
+	/* The below values were taken from the Android kernel */
+	PROPERTY_ENTRY_U32("precharge-current-microamp", 256000),
+	PROPERTY_ENTRY_U32("charge-term-current-microamp", 128000),
+	PROPERTY_ENTRY_U32("constant-charge-current-max-microamp", 2240000),
+	PROPERTY_ENTRY_U32("constant-charge-voltage-max-microvolt", 4192000),
+	PROPERTY_ENTRY_U32("factory-internal-resistance-micro-ohms", 150000),
+	{ }
+};
+
+const struct software_node acer_a1_840_4v20_battery_node = {
+	.properties = acer_a1_840_4v20_battery_props,
+};
+
+static const struct property_entry acer_a1_840_bq24190_props[] = {
+	PROPERTY_ENTRY_REF("monitored-battery", &acer_a1_840_4v20_battery_node),
+	PROPERTY_ENTRY_BOOL("omit-battery-class"),
+	PROPERTY_ENTRY_BOOL("disable-reset"),
+	{ }
+};
+
+static const struct software_node acer_a1_840_bq24190_node = {
+	.properties = acer_a1_840_bq24190_props,
+};
+
+static const struct property_entry acer_a1_840_touchscreen_props[] = {
+	PROPERTY_ENTRY_U32("touchscreen-size-x", 800),
+	PROPERTY_ENTRY_U32("touchscreen-size-y", 1280),
+	{ }
+};
+
+static const struct software_node acer_a1_840_touchscreen_node = {
+	.properties = acer_a1_840_touchscreen_props,
+};
+
+static const struct x86_i2c_client_info acer_a1_840_i2c_clients[] __initconst = {
+	{
+		/* BQ24297 charger IC */
+		.board_info = {
+			.type = "bq24297",
+			.addr = 0x6b,
+			.dev_name = "bq24297",
+			.swnode = &acer_a1_840_bq24190_node,
+			.platform_data = &bq24190_pdata,
+		},
+		.adapter_path = "\\_SB_.I2C1",
+		.irq_data = {
+			.type = X86_ACPI_IRQ_TYPE_GPIOINT,
+			.chip = "INT33FC:02",
+			.index = 2,
+			.trigger = ACPI_EDGE_SENSITIVE,
+			.polarity = ACPI_ACTIVE_LOW,
+			.con_id = "bq24297_irq",
+		},
+	}, {
+		/* MPU6515 sensors */
+		.board_info = {
+			.type = "mpu6515",
+			.addr = 0x69,
+			.dev_name = "mpu6515",
+		},
+		.adapter_path = "\\_SB_.I2C3",
+		.irq_data = {
+			.type = X86_ACPI_IRQ_TYPE_APIC,
+			.index = 0x47,
+			.trigger = ACPI_EDGE_SENSITIVE,
+			.polarity = ACPI_ACTIVE_HIGH,
+		},
+	}, {
+		/* FT5416 touchscreen controller */
+		.board_info = {
+			.type = "edt-ft5x06",
+			.addr = 0x38,
+			.dev_name = "ft5416",
+			.swnode = &acer_a1_840_touchscreen_node,
+		},
+		.adapter_path = "\\_SB_.I2C4",
+		.irq_data = {
+			.type = X86_ACPI_IRQ_TYPE_APIC,
+			.index = 0x45,
+			.trigger = ACPI_EDGE_SENSITIVE,
+			.polarity = ACPI_ACTIVE_HIGH,
+		},
+	}
+};
+
+struct gpiod_lookup_table acer_a1_840_int3496_gpios = {
+	.dev_id = "intel-int3496",
+	.table = {
+		GPIO_LOOKUP("INT33FC:02", 1, "mux", GPIO_ACTIVE_HIGH),
+		GPIO_LOOKUP("INT33FC:02", 18, "id", GPIO_ACTIVE_HIGH),
+		{ }
+	},
+};
+
+static struct gpiod_lookup_table acer_a1_840_ft5416_gpios = {
+	.dev_id = "i2c-ft5416",
+	.table = {
+		GPIO_LOOKUP("INT33FC:01", 26, "reset", GPIO_ACTIVE_LOW),
+		{ }
+	},
+};
+
+static struct gpiod_lookup_table acer_a1_840_fuel_gauge_gpios = {
+	.dev_id = "chtdc_ti_battery",
+	.table = {
+		GPIO_LOOKUP("INT33FC:02", 10, "charged", GPIO_ACTIVE_HIGH),
+		{ }
+	},
+};
+
+static struct gpiod_lookup_table * const acer_a1_840_gpios[] = {
+	&acer_a1_840_int3496_gpios,
+	&acer_a1_840_ft5416_gpios,
+	&acer_a1_840_fuel_gauge_gpios,
+	NULL
+};
+
+/* Properties for the Dollar Cove TI PMIC battery MFD child used as fuel-gauge */
+static const struct property_entry acer_a1_840_fuel_gauge_props[] = {
+	PROPERTY_ENTRY_REF("monitored-battery", &acer_a1_840_4v20_battery_node),
+	PROPERTY_ENTRY_STRING_ARRAY_LEN("supplied-from", bq24190_psy, 1),
+	{ }
+};
+
+static int __init acer_a1_840_init(struct device *dev)
+{
+	struct device *bat_dev;
+	int ret;
+
+	bat_dev = bus_find_device_by_name(&platform_bus_type, NULL, "chtdc_ti_battery");
+	if (!bat_dev)
+		return dev_err_probe(dev, -EPROBE_DEFER, "getting chtdc_ti_battery dev\n");
+
+	ret = device_create_managed_software_node(bat_dev, acer_a1_840_fuel_gauge_props, NULL);
+	put_device(bat_dev);
+
+	return ret;
+}
+
+const struct x86_dev_info acer_a1_840_info __initconst = {
+	.i2c_client_info = acer_a1_840_i2c_clients,
+	.i2c_client_count = ARRAY_SIZE(acer_a1_840_i2c_clients),
+	.pdev_info = int3496_pdevs,
+	.pdev_count = 1,
+	.gpiod_lookup_tables = acer_a1_840_gpios,
+	.bat_swnode = &acer_a1_840_4v20_battery_node,
+	.init = acer_a1_840_init,
+};
+
 /* Acer Iconia One 7 B1-750 has an Android factory image with everything hardcoded */
 static const char * const acer_b1_750_mount_matrix[] = {
 	"-1", "0", "0",
