a0c9dc8710618e79e9e9e5016e3633fac562aa9a
Hans de Goede <hansg@kernel.org>
2025-05-25 20:09:54 +0200

    media: mt9m114: Put sensor in reset on power-down
    Put sensor back in reset on power-down.
    
    Signed-off-by: Hans de Goede <hansg@kernel.org>
    ---
    Changes in v2
    - After setting reset high wait 20 clk cycles before disabling
      the clk and regulators

--- drivers/media/i2c/mt9m114.c
+++ drivers/media/i2c/mt9m114.c
@@ -2207,6 +2207,13 @@ error_regulator:
 
 static void mt9m114_power_off(struct mt9m114 *sensor)
 {
+	unsigned int duration;
+
+	gpiod_set_value(sensor->reset, 1);
+	/* Power off takes 10 clock cycles. Double it to be safe. */
+	duration = DIV_ROUND_UP(2 * 10 * 1000000, sensor->clk_freq);
+	fsleep(duration);
+
 	clk_disable_unprepare(sensor->clk);
 	regulator_bulk_disable(ARRAY_SIZE(sensor->supplies), sensor->supplies);
 }
