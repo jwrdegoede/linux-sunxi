46a26e1743896fcea96739f258cae18e982a067a
Hans de Goede <johannes.goede@oss.qualcomm.com>
2026-06-05 18:19:45 +0200

    clk: qcom: dispcc-sc7180: Stop disp_cc_mdss_mdp_clk_src from getting parked FIXME
    FIXME needs testing and figure out which of the 4 candidates exactly need this.
    
    Parking disp_cc_mdss_mdp_clk_src at 19.2MHz causing the EFI GOP framebuffer
    to stop functioning. The EFI GOP framebuffer should keep working until
    the msm display driver loads, to help with boot debugging and to ensure
    display output when the msm module is not in the initramfs.
    
    Switch disp_cc_mdss_mdp_clk_src over to clk_rcg2_shared_no_init_park_ops
    to keep the EFI GOP working after binding the dispcc-sc7180 driver.
    
    Signed-off-by: Hans de Goede <johannes.goede@oss.qualcomm.com>

--- drivers/clk/qcom/dispcc-sc7180.c
+++ drivers/clk/qcom/dispcc-sc7180.c
@@ -155,7 +155,7 @@ static struct clk_rcg2 disp_cc_mdss_ahb_clk_src = {
 		.parent_data = disp_cc_parent_data_4,
 		.num_parents = ARRAY_SIZE(disp_cc_parent_data_4),
 		.flags = CLK_SET_RATE_PARENT,
-		.ops = &clk_rcg2_shared_ops,
+		.ops = &clk_rcg2_shared_no_init_park_ops,
 	},
 };
 
@@ -264,7 +264,7 @@ static struct clk_rcg2 disp_cc_mdss_mdp_clk_src = {
 		.name = "disp_cc_mdss_mdp_clk_src",
 		.parent_data = disp_cc_parent_data_3,
 		.num_parents = ARRAY_SIZE(disp_cc_parent_data_3),
-		.ops = &clk_rcg2_shared_ops,
+		.ops = &clk_rcg2_shared_no_init_park_ops,
 	},
 };
 
@@ -292,7 +292,7 @@ static struct clk_rcg2 disp_cc_mdss_rot_clk_src = {
 		.name = "disp_cc_mdss_rot_clk_src",
 		.parent_data = disp_cc_parent_data_3,
 		.num_parents = ARRAY_SIZE(disp_cc_parent_data_3),
-		.ops = &clk_rcg2_shared_ops,
+		.ops = &clk_rcg2_shared_no_init_park_ops,
 	},
 };
 
@@ -306,7 +306,7 @@ static struct clk_rcg2 disp_cc_mdss_vsync_clk_src = {
 		.name = "disp_cc_mdss_vsync_clk_src",
 		.parent_data = disp_cc_parent_data_0,
 		.num_parents = ARRAY_SIZE(disp_cc_parent_data_0),
-		.ops = &clk_rcg2_shared_ops,
+		.ops = &clk_rcg2_shared_no_init_park_ops,
 	},
 };
 
