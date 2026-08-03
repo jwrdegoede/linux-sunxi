dd892aadcba4e48bda3411ca85133f0cb8b25b63
Luka Gejak <luka.gejak@linux.dev>
2026-08-03 07:51:47 +0000

    wifi: rtw88: 8723b: add the RTL8723B chip driver
    Add the Realtek RTL8723B 802.11n chip driver: the chip operations, the
    power sequences, the efuse layout, the RF and IQ calibration, and the
    chip specific coexistence handling.
    
    The RTL8723B chip support is based on the initial work by
    Michael Straube <straube.linux@gmail.com>.
    Link: https://github.com/mistraube/rtw88/tree/rtl8723bs
    
    Co-developed-by: Michael Straube <straube.linux@gmail.com>
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Signed-off-by: Luka Gejak <luka.gejak@linux.dev>

---
+++ drivers/net/wireless/realtek/rtw88/rtw8723b.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
+/*
+ * Copyright(c) 2026 Realtek Corporation
+ */
+
+#ifndef __RTW8723B_H__
+#define __RTW8723B_H__
+
+#include "rtw8723x.h"
+
+extern const struct rtw_chip_info rtw8723b_hw_spec;
+
+#endif
